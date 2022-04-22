2b9e1abd230c548f97d8d537519d720532bad916
Viktor Niderman <viktor.n@newizze.com>
2022-04-22 21:19:16 +0300

    add bootstrap

--- resources/js/APIListPage.vue
+++ resources/js/APIListPage.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
     <h2>Here is IPR-3 API</h2>
-    <ul>
-      <li v-for="el in API_DB">
-        {{el.method}} (<mark><code>{{backendUrl}}</code></mark>) - {{el.name}}
-      </li>
-    </ul>
+
+    <div class="card" v-for="el in API_DB">
+      <h5 class="card-header">{{el.name}}</h5>
+      <div class="card-body" v-for="met in el.methods">
+        <h5 class="card-title">{{met.name}}</h5>
+        <div class="card-text">
+          <p>
+            {{met.method}} - {{met.name}}
+            <br>
+            <mark><code>{{backendUrl}}{{met.url}}</code></mark>
+          </p>
+        </div>
+        <button disabled class="btn btn-primary">Try (in progress)</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,9 +27,14 @@ export default {
     return {
       API_DB: [
         {
-          url: '/api/v1/',
-          method: 'GET',
-          name: 'Check API'
+          name: 'Test Zone: Test API work',
+          methods: [
+            {
+              url: '/api/v1/',
+              method: 'GET',
+              name: 'Check API is working',
+            }
+          ],
         }
       ]
     }
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,18 @@
+
+
 require('./bootstrap');
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import BootstrapVue3 from 'bootstrap-vue-3'
 
-createApp({
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
+
+const app = createApp({
   components: {'app': App},
-}).mount('#app')
+});
+app.use(BootstrapVue3)
+app.mount('#app')
